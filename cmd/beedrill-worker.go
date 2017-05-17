d7413c81f3c6cac3751c9990ac4db66afaffc2df
Ibrar ASGHAR <ibrar.asghar@alterway.fr>
2017-05-17 10:25:03 +0200

    Adaptation of Beedrill to a new version of sysbench

--- cmd/beedrill-worker.go
+++ cmd/beedrill-worker.go
@@ -1,66 +1,105 @@
 package main
 
 import (
-	"flag"
+    "fmt"
 
 	"git.rnd.alterway.fr/beedrill/pkg"
+
 	machinery "github.com/RichardKnop/machinery/v1"
 	"github.com/RichardKnop/machinery/v1/config"
-	"github.com/RichardKnop/machinery/v1/errors"
+	//"github.com/RichardKnop/machinery/v1/log"
+	//"github.com/RichardKnop/machinery/v1/tasks"
+	"github.com/urfave/cli"
 )
 
 // Define flags
 var (
-	configPath    = flag.String("c", "config_beedrill-worker.yml", "Path to a configuration file")
-	broker        = flag.String("b", "redis://redis:6379/", "Broker URL")
-	resultBackend = flag.String("r", "redis://redis:6379/", "Result backend")
-	defaultQueue  = flag.String("q", "machinery_tasks", "Ephemeral Redis queue name")
-
-	cnf    config.Config
-	server *machinery.Server
-	worker *machinery.Worker
+	app           *cli.App
+	configPath    string
+	broker        string
+	resultBackend string
+	defaultQueue  string
 )
 
 func init() {
-	// Parse the flags
-	flag.Parse()
+	app = cli.NewApp()
 
-	cnf = config.Config{
-		Broker:        *broker,
-		ResultBackend: *resultBackend,
-		DefaultQueue:  *defaultQueue,
+	app.Name    = "Beedrill"
+	app.Usage   = "sysbench tasks sent with machinery to workers"
+	app.Author  = "Alter Way"
+	app.Email   = "qqch@alterway.fr"
+	app.Version = "0.1.0"
+
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:        "c",
+			Value:       "config_beedrill-worker.yml",
+			Destination: &configPath,
+			Usage:       "Path to a configuration file",
+		},
+		cli.StringFlag{
+			Name:        "b",
+			Value:       "redis://redis:6379/",
+			Destination: &broker,
+			Usage:       "Broker URL",
+		},
+		cli.StringFlag{
+			Name:        "r",
+			Value:       "redis://redis:6379/",
+			Destination: &resultBackend,
+			Usage:       "Result backend",
+		},
+		cli.StringFlag{
+			Name:        "q",
+			Value:       "machinery_tasks",
+			Destination: &defaultQueue,
+			Usage:       "Ephemeral Redis queue name",
+		},
+	}
+}
+
+func startServer() (*machinery.Server, error) {
+	cnf := config.Config{
+		Broker:        broker,
+		DefaultQueue:  defaultQueue,
+		ResultBackend: resultBackend,
 	}
 
-	// Parse the config
-	// NOTE: If a config file is present, it has priority over flags
-	data, err := config.ReadFromFile(*configPath)
-	if err == nil {
-		err = config.ParseYAMLConfig(&data, &cnf)
-		errors.Fail(err, "Could not parse config file")
+	// If present, the config file takes priority over cli flags
+	data, err := config.ReadFromFile(configPath)
+	if err != nil {
+		log.WARNING.Printf("Could not load config from file: %s", err.Error())
+	} else {
+		if err = config.ParseYAMLConfig(&data, &cnf); err != nil {
+			return nil, fmt.Errorf("Could not parse config file: %s", err.Error())
+		}
 	}
 
-	server, err = machinery.NewServer(&cnf)
-	errors.Fail(err, "Could not initialize server")
+	// Create server instance
+	server, err := machinery.NewServer(&cnf)
+	if err != nil {
+		return nil, fmt.Errorf("Could not initialize server: %s", err.Error())
+	}
 
 	// Register tasks
 	tasks := map[string]interface{}{
-		"add":         exampletasks.Add,
-		"multiply":    exampletasks.Multiply,
-		"panic_task":  exampletasks.PanicTask,
-		"simple_test": exampletasks.SimpleTest,
-		"sleep":       exampletasks.RestfulSleep,
-		"get_busy":    exampletasks.GetBusy,
-		"TCP_socket":  exampletasks.OperateTCP,
-		"task_args":   exampletasks.TaskArgs,
+		"task_args": beedrilltasks.TaskArgs,
 	}
+
 	server.RegisterTasks(tasks)
 
-	// The second argument is a consumer tag
-	// Ideally, each worker should have a unique tag (worker1, worker2 etc)
-	worker = server.NewWorker("machinery_worker")
+	return server, nil
 }
 
 func main() {
-	err := worker.Launch()
-	errors.Fail(err, "Could not launch worker")
+    server, err := startServer()
+	if err != nil {
+		fmt.Errorf("Could not start the worker server", err.Error())
+	}
+
+	worker := server.NewWorker("beedrill_worker")
+
+	if err := worker.Launch(); err != nil {
+		fmt.Errorf("Could not launch the worker", err.Error())
+	}
 }
--- pkg/tasks.go
+++ pkg/tasks.go
@@ -1,4 +1,4 @@
-package exampletasks
+package beedrilltasks
 
 import (
 	"errors"
